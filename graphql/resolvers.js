4220a1e5695a4916ade4aaa7842a06b487f08632
Sara Anwar <saraanwar97@gmail.com>
2021-07-21 22:29:18 +0200

    fixed a bug

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -97,21 +97,22 @@ module.exports = {
              updatedAt: createdPost.updatedAt.toISOString()};
 
     },
-    getPosts:  async function(args,req){
+    getPosts: async function(args,req){
         if(!req.isAuth){
             const error = new Error('Not authenticated!');
             error.code = 401;
             throw error;
         }
         const totalPosts = await Post.find().countDocuments();
-        const posts = await Post.find().sort({ createdAt: -1 }).populate(creator);
+        const posts = await Post.find().sort({ createdAt: -1 }).populate('creator');
         return {posts:posts.map(p=>{
             return {...p._doc,
-                _id:createdPost._id.toString(),
-                createdAt: createdPost.createdAt.toISOString() ,
-                updatedAt: createdPost.updatedAt.toISOString()
+                _id:p._id.toString(),
+                createdAt: p.createdAt.toISOString() ,
+                updatedAt: p.updatedAt.toISOString()
             };
-        }), totalPosts:totalPosts};
+        }), totalPosts:totalPosts
+    };
     }
 
 };
