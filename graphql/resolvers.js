be157a0e203ff1808bada7682c9b6f28550e29c4
Sara Anwar <saraanwar97@gmail.com>
2021-07-21 14:54:25 +0200

    added a create post mutation

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -2,6 +2,7 @@ const User = require('../models/user');
 const bcrypt = require('bcryptjs');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
+const Post = require('../models/post');
 
 module.exports = {
 
@@ -55,6 +56,30 @@ module.exports = {
       {expiresIn: '1h' }
       );
       return {token:token , userId: user._id.toString()};
+    },
+    createPost: async function ({ postInput }, req){
+        const errors = [];
+        if( validator.isEmpty(postInput.title) || !validator.isLength(postInput.title, {min : 5}) ){
+            errors.push({message: 'title is invalid '});
+        }
+        if( validator.isEmpty(postInput.content) || !validator.isLength(postInput.content, {min : 5}) ){
+            errors.push({message: 'content is invalid '});
+        }
+        if (errors.length > 0){
+            const error = new Error ('Invalid input');
+            error.data = errors;
+            error.code = 422; // can use my own status coding system
+            throw error;
+        }
+        const post = new Post({
+            title: postInput.title,
+            content: postInput.content,
+            imageUrl: postInput.imageUrl
+
+        });
+        const createdPost = await post.save();
+        return {...createdPost._doc, _id:createdPost._id.toString(),createdAt: createdPost.createdAt.toISOString() , updatedAt: createdPost.updatedAt.toISOString()};
+
     }
 
 };
