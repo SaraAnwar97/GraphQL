5507e001e7a732bbb6cc1908970bf40955093834
Sara Anwar <saraanwar97@gmail.com>
2021-07-23 00:50:29 +0200

    deleting a post

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcryptjs');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const Post = require('../models/post');
+const {clearImage} = require('../util/file'); 
 
 module.exports = {
 
@@ -185,6 +186,30 @@ module.exports = {
             createdAt: updatedPost.createdAt.toISOString() ,
             updatedAt: updatedPost.updatedAt.toISOString()
         };
+    },
+    deletePost: async function ({id}, req){
+        if(!req.isAuth){
+            const error = new Error('Not authenticated!');
+            error.code = 401;
+            throw error;
+        }
+        const post = await Post.findById(id);
+        if(!post){
+            const error = new Error('No post found');
+            error.code = 404;
+            throw error;
+        }
+        //checking if user who wants to edit the post is the one who created it
+        if(post.creator.toString() !== req.userId.toString()){
+            const error = new Error('Not authorized');
+            error.code = 403;
+            throw error;
+        }
+        clearImage(post.imageUrl);
+        await Post.findByIdAndRemove(id);
+        const user = await User.findById(req.userId);
+        user.posts.pull(id);
+        await user.save();
+        return true;
     }
-
 };
